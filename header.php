059bf0e995f71ae2d4f1225b3b4f724759b3b609
Shira Zacks <shiramzacks@gmail.com>
2023-09-28 22:51:27 -0400

    adding stuff

--- header.php
+++ header.php
@@ -1,3 +1,23 @@
-<div>
-    
-</div>
+<?php
+echo ' <!-- Navbar  -->
+    <div class="top">
+        <div class="topBar">
+        <a href="index.php" class="logo"><img src="littlethings_logo.png"></a>
+
+            <div class="smaller">
+                <a class="bar" onclick="myFunction()" title="Toggle Navigation Menu"><img src="bars.svg" alt="MENU"></a>
+            </div>
+            <a href="parsha.php" class="menuButton">This Week\'s Parsha</a>
+        </div>
+
+        <!-- Navbar on small screens -->
+        <div id="smallScreen" class="hide">
+            <ul>
+                <li><a href="index.php" class="dropdownButton top">Home</a> </li>
+                <li><a href="parsha.php" class="dropdownButton">This Week\'s Parsha</a> </li>
+                <li><a href="geula.php" class="dropdownButton bottom">Bring the Geula</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="under_the_menu"></div>
+';
--- index.php
+++ index.php
@@ -5,12 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Little Things</title>
     <link rel="stylesheet" type="text/css" href="reset.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Della+Respira&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="icon" type="image/png" href="happiness.png">
-
 </head>
-
+<?php include "header.php"?>
 <body>
+
+
+
     <h2>This is a parked page for the upcoming website. Woohoo!! 🥳</h2>
 </body>
 </html>
