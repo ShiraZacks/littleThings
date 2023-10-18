4773184bb7da0d43788ee37ac8dfe2b0afe5e595
Shira Zacks <shiramzacks@gmail.com>
2023-10-18 11:41:38 -0400

    added style and content

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <div class="top">
-    <div class="topBar container">
-        <a href="index.php"></a><img id="logo_flower" src="flowers/flower_4.png"></a>
+    <div class="container">
+        <img id="logo_flower" src="flowers/flower_4.png">
+        <a href="index.php" class="menuButton"><span class="spacerSpan">〖</span>Home<span class="spacerSpan">〗</span></a>
         <a href="addLittleThing.php" class="menuButton"><span class="spacerSpan">〖</span>Today's Thing<span class="spacerSpan">〗</span></a>
         <a href="dailyLittleThing.php" class="menuButton"><span class="spacerSpan">〖</span>Add a Thing<span class="spacerSpan">〗</span></a>
-
     </div>
 </div>
-<div class="under_the_menu"></div>
+<div id="under_the_menu"></div>
--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-	document.getElementById("smallScreen").classList.toggle("show");
-}
+// function myFunction() {
+// 	// console.log(document.getElementById("top").style.height);
+
+// 	let topBar = document.getElementById("topBar").offsetHeight;
+// 	let height = topBar + "px";
+// 	console.log(height)
+// 	// document.getElementById("under_the_menu").style.height = height;
+
+
+
+// 	var r = document.querySelector(':root');
+
+// 	// Get the styles (properties and values) for the root
+// 	var rs = getComputedStyle(r);
+// 	// Alert the value of the --blue variable
+// 	console.log("top bar height is " + rs.getPropertyValue('--topBarHeight'));
+
+// 	// Create a function for setting a variable value
+// 	// Set the value of variable --blue to another value (in this case "lightblue")
+// 	r.style.setProperty('--blue', height);
+
+// };
+// myFunction();
